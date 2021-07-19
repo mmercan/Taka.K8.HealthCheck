858fe907c283f8c72c82e5eedf26cbacd20370d0
Murat MERCAN <mmercan@outlook.com>
2021-07-20 00:10:10 +1000

    output format moved to opencover

--- Workers/Taka.Worker.Sync/docker-entrypoint.sh
+++ Workers/Taka.Worker.Sync/docker-entrypoint.sh
@@ -7,7 +7,7 @@ dotnet build ./Workers/Taka.Worker.Sync/Taka.Worker.Sync.sln
 # dotnet test ./Empty.Tests/Empty.Tests.sln /p:CollectCoverage=true /p:MergeWith="/TestResults/coverage.json" /p:CoverletOutputFormat="opencover" /p:CoverletOutput=/TestResults/
 
 dotnet test ./Workers/Taka.Worker.Sync/Taka.Worker.Sync.sln  /p:CollectCoverage=true /p:Exclude="[xunit.*.*]*" /p:CoverletOutput=/TestResults/ /p:MergeWith=/TestResults/coverage.json --logger=trx -r /TestResults/
-dotnet test ./Empty.Tests/Empty.Tests.sln /p:CollectCoverage=true /p:MergeWith="/TestResults/coverage.json" /p:CoverletOutputFormat="cobertura" /p:CoverletOutput=/TestResults/ 
+dotnet test ./Empty.Tests/Empty.Tests.sln /p:CollectCoverage=true /p:MergeWith="/TestResults/coverage.json" /p:CoverletOutputFormat="opencover" /p:CoverletOutput=/TestResults/ 
 
 dotnet build-server shutdown
 dotnet sonarscanner end /d:sonar.login=$SONARKEY
