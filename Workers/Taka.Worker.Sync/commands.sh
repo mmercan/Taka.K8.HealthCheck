9c8aa1475f1d665567d84330a3b274df0f9d6bdd
Murat MERCAN <mmercan@outlook.com>
2021-07-19 21:13:24 +1000

    test moved to docker run

---
+++ Workers/Taka.Worker.Sync/commands.sh
@@ -0,0 +1,13 @@
+dotnet sonarscanner begin /k:$PROJECT /v:$VERSION /d:sonar.host.url=$SONAR_HOST /o:mmercan-github /d:sonar.login=$SONARKEY  /d:sonar.cs.opencover.reportsPaths="/TestResults/coverage.opencover.xml" /d:sonar.exclusions="**bootstrap.css, **bootstrap-reboot.css, **bootstrap.js, **/wwwroot/**, **Empty.Tests" /d:sonar.coverage.exclusions="**Tests*.cs, **.js," /d:sonar.cs.vstest.reportsPaths="/TestResults/*.trx"
+dotnet build ./Workers/Taka.Worker.Sync/Taka.Worker.Sync.sln
+
+# dotnet test ./Workers/Taka.Worker.Sync/Taka.Worker.Sync.sln   /p:CollectCoverage=true /p:CoverletOutput=/TestResults/ /p:MergeWith=/TestResults/coverage.json --logger=trx -r /TestResults/
+# dotnet test ./Empty.Tests/Empty.Tests.sln /p:CollectCoverage=true /p:MergeWith="/TestResults/coverage.json" /p:CoverletOutputFormat="opencover" /p:CoverletOutput=/TestResults/
+
+dotnet test ./Workers/Taka.Worker.Sync/Taka.Worker.Sync.sln  /p:CollectCoverage=true /p:Exclude="[xunit.*.*]*" /p:CoverletOutput=/TestResults/ /p:MergeWith=/TestResults/coverage.json --logger=trx -r /TestResults/
+dotnet test ./Empty.Tests/Empty.Tests.sln /p:CollectCoverage=true /p:MergeWith="/TestResults/coverage.json" /p:CoverletOutputFormat="cobertura" /p:CoverletOutput=/TestResults/ 
+
+dotnet build-server shutdown
+dotnet sonarscanner end /d:sonar.login=$SONARKEY
+
+ 
