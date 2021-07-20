c89cfa8d2095636f58925c9f5971e0fa51a2665d
Murat MERCAN <mmercan@outlook.com>
2021-07-20 15:50:00 +1000

    SetTcpKeepAlives moved to a helper static class

--- Libs/Taka.K8s/KubernetesClient.cs
+++ Libs/Taka.K8s/KubernetesClient.cs
@@ -13,18 +13,9 @@ namespace Taka.K8s
         Kubernetes Client { get; }
     }
 
-
-    public class KubernetesClientFromConfigFile : IKubernetesClient
+    public static class KubernetesClientHelper
     {
-        public Kubernetes Client { get; }
-        public KubernetesClientFromConfigFile()
-        {
-            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
-            Client = new Kubernetes(config);
-            SetTcpKeepAlives(Client);
-        }
-
-        private void SetTcpKeepAlives(k8s.Kubernetes client)
+        public static void SetTcpKeepAlives(k8s.Kubernetes client)
         {
             var realHandler = client.HttpMessageHandlers.FirstOrDefault(h => !(h is DelegatingHandler));
             if (!(realHandler is HttpClientHandler))
@@ -56,6 +47,19 @@ namespace Taka.K8s
         }
     }
 
+    public class KubernetesClientFromConfigFile : IKubernetesClient
+    {
+        public Kubernetes Client { get; }
+        public KubernetesClientFromConfigFile()
+        {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            Client = new Kubernetes(config);
+            KubernetesClientHelper.SetTcpKeepAlives(Client);
+        }
+
+
+    }
+
 
     public class KubernetesClientInClusterConfig : IKubernetesClient
     {
@@ -64,74 +68,39 @@ namespace Taka.K8s
         {
             var config = KubernetesClientConfiguration.InClusterConfig();
             Client = new Kubernetes(config);
-            {
-                var hf = typeof(HttpMessageInvoker).GetField("_handler", BindingFlags.Instance | BindingFlags.NonPublic);
-                var h = hf.GetValue(Client.HttpClient);
-                while (!(h is HttpClientHandler))
-                {
-                    h = ((DelegatingHandler)h).InnerHandler;
-                }
-                var sh = new SocketsHttpHandler();
-                sh.ConnectCallback = async (context, token) =>
-                {
-                    var socket = new Socket(SocketType.Stream, ProtocolType.Tcp)
-                    {
-                        NoDelay = true,
-                    };
-                    socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
-                    var ip = Dns.GetHostEntry(context.DnsEndPoint.Host).AddressList.First();
-                    var ep = new IPEndPoint(ip, context.DnsEndPoint.Port);
-                    // Console.WriteLine(ep);
-                    await socket.ConnectAsync(ep, token).ConfigureAwait(false);
-                    return new NetworkStream(socket, ownsSocket: true);
-                };
-                var p = h.GetType().GetField("_underlyingHandler", BindingFlags.NonPublic | BindingFlags.Instance);
-                p.SetValue(h, (sh));
-            }
+            KubernetesClientHelper.SetTcpKeepAlives(Client);
 
-            {
-                var m = Client.GetType().GetMethod("InitializeFromConfig", BindingFlags.NonPublic | BindingFlags.Instance);
-                m.Invoke(Client, new[] { config });
-            }
+            // Client = new Kubernetes(config);
+            // {
+            //     var hf = typeof(HttpMessageInvoker).GetField("_handler", BindingFlags.Instance | BindingFlags.NonPublic);
+            //     var h = hf.GetValue(Client.HttpClient);
+            //     while (!(h is HttpClientHandler))
+            //     {
+            //         h = ((DelegatingHandler)h).InnerHandler;
+            //     }
+            //     var sh = new SocketsHttpHandler();
+            //     sh.ConnectCallback = async (context, token) =>
+            //     {
+            //         var socket = new Socket(SocketType.Stream, ProtocolType.Tcp)
+            //         {
+            //             NoDelay = true,
+            //         };
+            //         socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+            //         var ip = Dns.GetHostEntry(context.DnsEndPoint.Host).AddressList.First();
+            //         var ep = new IPEndPoint(ip, context.DnsEndPoint.Port);
+            //         // Console.WriteLine(ep);
+            //         await socket.ConnectAsync(ep, token).ConfigureAwait(false);
+            //         return new NetworkStream(socket, ownsSocket: true);
+            //     };
+            //     var p = h.GetType().GetField("_underlyingHandler", BindingFlags.NonPublic | BindingFlags.Instance);
+            //     p.SetValue(h, (sh));
+            // }
 
+            // {
+            //     var m = Client.GetType().GetMethod("InitializeFromConfig", BindingFlags.NonPublic | BindingFlags.Instance);
+            //     m.Invoke(Client, new[] { config });
+            // }
 
-            //SetTcpKeepAlives(Client);
-
-
-
-
-        }
-
-
-        private void SetTcpKeepAlives(k8s.Kubernetes client)
-        {
-            var realHandler = client.HttpMessageHandlers.FirstOrDefault(h => !(h is DelegatingHandler));
-            if (!(realHandler is HttpClientHandler))
-            {
-                throw new Exception("Expected HttpClientHandler");
-            }
-
-            var underlyingHandlerProperty = realHandler.GetType().GetField("_underlyingHandler", BindingFlags.NonPublic | BindingFlags.Instance);
-            if (underlyingHandlerProperty == null)
-            {
-                throw new Exception("Expected _underlyingHandler property not found.");
-            }
-
-            var underlyingHandler = underlyingHandlerProperty.GetValue(realHandler);
-            if (underlyingHandler == null)
-            {
-                throw new Exception("_underlyingHandler is null.");
-            }
-
-            if (underlyingHandler is SocketsHttpHandler socketHandler)
-            {
-                // we reached the SocketsHttpHandler, enable the keepalive delay.
-                socketHandler.KeepAlivePingDelay = TimeSpan.FromSeconds(10);
-            }
-            else
-            {
-                throw new Exception($"Expected to find SocketsHttpHandler, but found: {underlyingHandler.GetType().Name}");
-            }
         }
     }
 }
